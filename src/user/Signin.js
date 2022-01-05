d916e872a460ba8dc47ce0f195727f84c4f78cb9
Vikash-Pareek <vikash.pareek1509@gmail.com>
2022-01-06 01:02:19 +0530

    Signin and Signup page UI updated

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -41,17 +41,17 @@ export default function Signin() {
   };
 
   const showError = () => (
-    <div
-      className=''
+    <h2
+      className='auth-error-text'
       style={{ display: error ? '' : 'none' }}
     >
       {error}
-    </div>
+    </h2>
   );
 
   const showLoading = () =>
     loading && (
-      <div className=''>
+      <div className='loading-text'>
         <h2>Loading...</h2>
       </div>
     );
@@ -70,42 +70,40 @@ export default function Signin() {
   };
 
   const signInForm = () => (
-    <section>
-      {showError()}
-      {showLoading()}
-      {redirectUser()}
-      <div className="">
-        <div className="">
-          <LockOutlinedIcon />
-        </div>
-        <h1>Signin</h1>
-        <form>
-          <div>
-            <label>Email<span>*</span></label>
-            <input id='email' name='email' placeholder='Enter Email' autoComplete='email' onChange={handleChange('email')}
-              type='email' value={email} required />
-          </div>
-          <div>
-            <label>Password<span>*</span></label>
-            <input name='password' placeholder='Enter Password' type='password' id='password' onChange={handleChange('password')}
-              value={password} autoComplete='current-password' required />
-          </div>
-          <button onClick={clickSubmit} type='submit' className="">
-            Signin
-          </button>
-          <div>
-            <Link to='/signup'>
-              {"Don't have an account? Signup"}
-            </Link>
-          </div>
-        </form>
+    <div className='user-auth-form-container'>
+      <div className='user-auth-icon'>
+        <LockOutlinedIcon fontSize='large' />
       </div>
-    </section>
+      <h1>Signin</h1>
+      <form className='user-auth-form'>
+        <div className='form-field-container'>
+          <label for='email'>Email<span>*</span></label>
+          <input id='email' name='email' placeholder='Enter Email' autoComplete='email' onChange={handleChange('email')}
+            type='email' value={email} required />
+        </div>
+        <div className='form-field-container'>
+          <label for='password'>Password<span>*</span></label>
+          <input name='password' placeholder='Enter Password' type='password' id='password' onChange={handleChange('password')}
+            value={password} autoComplete='current-password' required />
+        </div>
+        <button onClick={clickSubmit} type='submit' className='user-auth-form-btn'>
+          Signin
+        </button>
+      </form>
+      <Link to='/signup'>
+        <p className='form-link-text'>Don't have an account? Signup</p>
+      </Link>
+    </div>
   );
 
   return (
     <Layout title='Enter to the world of FARMERLY.' path='/signin'>
-      {signInForm()}
+      <section id='user-auth-container'>
+        {showError()}
+        {showLoading()}
+        {redirectUser()}
+        {signInForm()}
+      </section>
       <Copyright />
     </Layout>
   );
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -13,10 +13,11 @@ export default function Signup() {
     email: '',
     password: '',
     error: '',
+    loading: false,
     success: false,
   });
 
-  const { name, email, password, success, error } = values;
+  const { name, email, password, success, error, loading } = values;
 
   const handleChange = (name) => (event) => {
     setValues({ ...values, error: false, [name]: event.target.value });
@@ -24,10 +25,10 @@ export default function Signup() {
 
   const clickSubmit = (event) => {
     event.preventDefault(); // so that browser does not reload
-    setValues({ ...values, error: false });
+    setValues({ ...values, error: false, loading: true });
     signup({ name, email, password }).then((data) => {
       if (data.error) {
-        setValues({ ...values, error: data.error, success: false });
+        setValues({ ...values, error: data.error, success: false, loading: false });
       } else {
         setValues({
           ...values,
@@ -35,6 +36,7 @@ export default function Signup() {
           email: '',
           password: '',
           error: '',
+          loading: false,
           success: true,
         });
       }
@@ -42,66 +44,70 @@ export default function Signup() {
   };
 
   const showError = () => (
-    <div
-      className=''
+    <h2
+      className='auth-error-text'
       style={{ display: error ? '' : 'none' }}
     >
       {error}
-    </div>
+    </h2>
   );
 
+  const showLoading = () =>
+    loading && (
+      <div className='loading-text'>
+        <h2>Loading...</h2>
+      </div>
+    );
+
   const showSuccess = () => (
-    <div
-      className=''
+    <h2
+      className='success-text'
       style={{ display: success ? '' : 'none' }}
     >
-      New account is created. Please <Link to='/signin'>Signin</Link>.
-    </div>
+      New account is created. Please <Link to='/signin'><span>Signin</span></Link>.
+    </h2>
   );
 
   const signUpForm = () => (
-    <section>
-      {showSuccess()}
-      {showError()}
-      <div className="">
-        <div className="">
-          <LockOutlinedIcon />
-        </div>
-        <h1>Signup</h1>
-        <form className="">
-          <div>
-            <div>
-              <label>Full Name<span>*</span></label>
-              <input autoComplete='off' onChange={handleChange('name')} type='text' name='name' placeholder='Enter Full Name'
-                value={name} id='name' required />
-            </div>
-            <div>
-              <label>Email<span>*</span></label>
-              <input id='email' name='email' placeholder='Enter Email' onChange={handleChange('email')} type='email'
-                value={email} autoComplete='off' required />
-            </div>
-            <div>
-              <label>Create Password<span>*</span></label>
-              <input name='password' placeholder='Enter Password' type='password' id='password' onChange={handleChange('password')}
-                value={password} autoComplete='current-password' required />
-            </div>
-          </div>
-          <button type='submit' className="" onClick={clickSubmit}>
-            Signup
-          </button>
-          <div>
-            <Link to='/signin'>
-              Already have an account? Signin
-            </Link>
-          </div>
-        </form>
+    <div className='user-auth-form-container'>
+      <div className='user-auth-icon'>
+        <LockOutlinedIcon fontSize='large' />
       </div>
-    </section>
+      <h1>Signup</h1>
+      <form className='user-auth-form'>
+        <div className='form-field-container'>
+          <label for='name'>Full Name<span>*</span></label>
+          <input autoComplete='off' onChange={handleChange('name')} type='text' name='name' placeholder='Enter Full Name'
+            value={name} id='name' required />
+        </div>
+        <div className='form-field-container'>
+          <label for='email'>Email<span>*</span></label>
+          <input id='email' name='email' placeholder='Enter Email' onChange={handleChange('email')} type='email'
+            value={email} autoComplete='off' required />
+        </div>
+        <div className='form-field-container'>
+          <label for='password'>Create Password<span>*</span></label>
+          <input name='password' placeholder='Enter Password' type='password' id='password' onChange={handleChange('password')}
+            value={password} autoComplete='current-password' required />
+        </div>
+        <button type='submit' className='user-auth-form-btn' onClick={clickSubmit}>
+          Signup
+        </button>
+      </form>
+      <Link to='/signin'>
+        <p className='form-link-text'>Already have an account? Signin</p>
+      </Link>
+    </div>
   );
 
   return (
-    <Layout title='Join the team of FARMERLY.' path='/signup'>
-      {signUpForm()}
+    <Layout title='Join the community of FARMERLY.' path='/signup'>
+      <section id='user-auth-container'>
+        {showSuccess()}
+        {showError()}
+        {showLoading()}
+        {signUpForm()}
+      </section>
       <Copyright />
     </Layout>
   );

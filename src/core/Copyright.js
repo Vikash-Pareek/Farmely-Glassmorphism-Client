815a6ffe13d85376b2b695a12d844318726c49de
Vikash-Pareek <vikash.pareek1509@gmail.com>
2022-01-06 20:47:21 +0530

    linkedin links added to the developers name in footer

--- src/core/Copyright.js
+++ src/core/Copyright.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./Copyright.css";
 
 export default function Copyright() {
   return (
     <footer id='copyright-container'>
       <p align='center'>
-        Farmerly &ensp;-&ensp; Copyright © {new Date().getFullYear()}<br />Website Developed By <span>Vikash Pareek</span> and <span>Umair Shaik</span>.
+        Farmerly &ensp;-&ensp; Copyright © {new Date().getFullYear()}<br />Website Developed By
+        &nbsp;
+        <Link to={{ pathname: 'https://www.linkedin.com/in/vikash-pareek/' }} target="_blank"><span>Vikash Pareek</span></Link>
+        &nbsp;
+        and
+        &nbsp;
+        <Link to={{ pathname: 'https://www.linkedin.com/in/umair-shaik-40849517a/' }} target="_blank"><span>Umair Shaik</span></Link>.
       </p>
     </footer>
   );
